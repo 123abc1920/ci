a6053369c9d0cd50fcdd9a60c2fb663096abede7
123abc1920 <123abc1920@mail.ru>
2026-02-17 20:53:03 +0300

    add dynamic memory

--- task1/task.cpp
+++ task1/task.cpp
@@ -9,13 +9,17 @@ int main()
     std::cin >> row;
     std::cin >> col;
 
-    int numbers[row][col];
+    int **numbers = new int *[row];
+    for (int i = 0; i < row; i++)
+    {
+        *(numbers + i) = new int[col];
+    }
 
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
         {
-            numbers[i][j] = 1 + rand() % 20;
+            *(*(numbers + i) + j) = 1 + rand() % 20;
         }
     }
 
@@ -23,29 +27,37 @@ int main()
     {
         for (int j = 0; j < col; j++)
         {
-            std::cout << numbers[i][j] << " ";
+            std::cout << *(*(numbers + i) + j) << " ";
         }
         std::cout << std::endl;
     }
 
-    int sums[col];
+    int *sums = new int[col];
     for (int i = 0; i < col; i++)
     {
-        sums[i] = 0;
+        *(sums + i) = 0;
     }
 
     for (int j = 0; j < col; j++)
     {
         for (int i = 0; i < row; i++)
         {
-            sums[j] += numbers[i][j];
+            *(sums + j) += *(*(numbers + i) + j);
         }
     }
 
     for (int i = 0; i < col; i++)
     {
-        std::cout << sums[i] << " ";
+        std::cout << *(sums + i) << " ";
     }
+
+    delete[] sums;
+
+    for (int i = 0; i < row; i++)
+    {
+        delete[] *(numbers + i);
+    }
+    delete[] numbers;
 
     return 0;
 }
