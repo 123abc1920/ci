180c4ec3e6a5a7e7a8f4ec119796c3c118932c8a
123abc1920 <123abc1920@mail.ru>
2026-03-30 18:53:00 +0300

    divide tests

--- task3/main.cpp
+++ task3/main.cpp
@@ -2,7 +2,13 @@
 #include <iostream>
 #include "theset.h"
 
-void intSet()
+void printSetInfo(TheSet<int> &set, const std::string &name)
+{
+    std::cout << name << ":" << std::endl;
+    set.print();
+}
+
+void basics()
 {
     TheSet<int> set;
 
@@ -10,58 +16,95 @@ void intSet()
     set.add(2);
     set.add(3);
 
-    std::cout << "Set 1:" << std::endl;
-    set.print();
+    printSetInfo(set, "Set 1");
 
     set.remove(2);
-
     std::cout << "Remove 2:" << std::endl;
     set.print();
 
-    std::cout << "Contain 1: " << set.contain(1) << ", Contain 2: " << set.contain(2) << std::endl;
+    std::cout << "Contain 1: " << set.contain(1)
+              << ", Contain 2: " << set.contain(2) << std::endl;
+}
+
+void sums()
+{
+    TheSet<int> set;
+    set.add(1);
+    set.add(2);
+    set.add(3);
 
     TheSet<int> set2;
-
     set2.add(4);
     set2.add(5);
     set2.add(6);
     set2.add(3);
 
-    std::cout << "Set 2:" << std::endl;
-    set2.print();
+    printSetInfo(set2, "Set 2");
 
-    std::cout << "set=set+st2: " << std::endl;
+    std::cout << "set=set+set2: " << std::endl;
     set = set + set2;
     set.print();
+}
+
+void subtractions()
+{
+    TheSet<int> set;
+    set.add(1);
+    set.add(2);
+    set.add(3);
+    set.add(4);
+    set.add(5);
+    set.add(6);
 
     TheSet<int> set3;
-
     set3.add(4);
     set3.add(6);
     set3.add(3);
 
-    std::cout << "Set 3:" << std::endl;
-    set3.print();
+    printSetInfo(set3, "Set 3");
 
-    std::cout << "set=set-st3: " << std::endl;
+    std::cout << "set=set-set3: " << std::endl;
     set = set - set3;
     set.print();
+}
 
-    std::cout << "set is empty: " << set.isEmpty() << std::endl;
+void comparing()
+{
+    TheSet<int> set;
+    set.add(1);
+    set.add(5);
 
-    TheSet<int> set4;
-    set4.add(1);
-    set4.add(5);
-    std::cout << "Set 4:" << std::endl;
-    set4.print();
+    TheSet<int> set2;
+    set2.add(1);
+    set2.add(2);
+    set2.add(3);
+    set2.add(4);
+    set2.add(5);
+    set2.add(6);
 
-    std::cout << "set4==set: " << (set4 == set) << std::endl;
+    printSetInfo(set, "Set 4");
+
+    std::cout << "set4==set: " << (set == set2) << std::endl;
 
     std::cout << "Clear set: " << std::endl;
-    set.clear();
-    std::cout << "Set is empty: " << set.isEmpty() << std::endl
+    set2.clear();
+    std::cout << "Set is empty: " << set2.isEmpty() << std::endl
               << "Set: " << std::endl;
-    set.print();
+    set2.print();
+}
+
+void intSet()
+{
+    basics();
+    std::cout << std::endl;
+
+    sums();
+    std::cout << std::endl;
+
+    subtractions();
+    std::cout << std::endl;
+
+    comparing();
 }
 
 void strSet()
