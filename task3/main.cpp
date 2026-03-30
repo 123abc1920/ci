2a0ae41c8bc52bce14558bfd9437194806f8abdb
123abc1920 <123abc1920@mail.ru>
2026-03-30 14:04:24 +0300

    add + - =

--- task3/main.cpp
+++ task3/main.cpp
@@ -32,6 +32,16 @@ public:
         content = new T[BASE_COUNT];
         length = BASE_COUNT;
     }
+    TheSet(const TheSet &other)
+    {
+        length = other.length;
+        count = other.count;
+        content = new T[length];
+        for (int i = 0; i < count; i++)
+        {
+            content[i] = other.content[i];
+        }
+    }
 
     ~TheSet()
     {
@@ -72,10 +82,78 @@ public:
                 content[i] = content[i + 1];
             }
         }
+        if (content[count - 1] == el)
+        {
+            shift = true;
+        }
         if (shift == true)
         {
             count--;
         }
+    }
+
+    const T *getAll() const
+    {
+        return content;
+    }
+
+    int size() const
+    {
+        return count;
+    }
+
+    TheSet &operator=(const TheSet &other)
+    {
+        if (this != &other)
+        {
+            delete[] content;
+            length = other.length;
+            count = other.count;
+            content = new T[length];
+            for (int i = 0; i < count; i++)
+            {
+                content[i] = other.content[i];
+            }
+        }
+        return *this;
+    }
+    TheSet operator+(const TheSet &other)
+    {
+        TheSet result = *this;
+
+        const T *newEls = other.getAll();
+        int size = other.size();
+
+        for (int i = 0; i < size; i++)
+        {
+            bool exists = false;
+            for (int j = 0; j < result.size(); j++)
+            {
+                if (result.content[j] == newEls[i])
+                {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists)
+            {
+                result.add(newEls[i]);
+            }
+        }
+        return result;
+    }
+    TheSet operator-(const TheSet &other)
+    {
+        TheSet result = *this;
+
+        const T *newEls = other.getAll();
+        int size = other.size();
+
+        for (int i = 0; i < size; i++)
+        {
+            result.remove(newEls[i]);
+        }
+        return result;
     }
 
     void print()
@@ -102,7 +180,32 @@ int main()
 
     set.print();
 
-    std::cout << set.contain(1) << " " << set.contain(2);
+    std::cout << set.contain(1) << " " << set.contain(2) << std::endl;
+
+    TheSet<int> set2;
+
+    set2.add(4);
+    set2.add(5);
+    set2.add(6);
+    set2.add(3);
+
+    set2.print();
+
+    set = set + set2;
+
+    set.print();
+
+    TheSet<int> set3;
+
+    set3.add(4);
+    set3.add(6);
+    set3.add(3);
+
+    set3.print();
+
+    set = set - set3;
+
+    set.print();
 
     return 0;
 }
